1e9907717755ff737a6c8301b9b8beb6f274e53c
CyrusN <CyrusN@CyrusN.local>
2017-07-05 23:20:55 +0800

    update voice server to accept option on startup

--- voiceServer/server.js
+++ voiceServer/server.js
@@ -1,16 +1,35 @@
-var googleTTS = require('google-tts-api');
-var express = require('express');
-var morgan = require('morgan')
-var app = express();
+const googleTTS = require('google-tts-api');
+const express = require('express');
+const morgan = require('morgan')
+const app = express();
+const logger = morgan('common')
 
-var logger = morgan('common')
+const yargs = require('yargs')
+const argv = require('yargs')
+  .option('port', {
+    alias: 'p',
+    default: 5001,
+    describe: 'choose a port',
+    type: 'number'
+  })
+  .option('speed', {
+    alias: 's',
+    describe: 'choose a speed',
+    default: 0.24,
+    type: 'number'
+  })
+  .help()
+  .argv
+
+const port = argv.port
+const speed = argv.speed
 
 app.use(logger)
 
 app.get('/voice/:title', function(req, res){
   const title = req.params.title
 
-  googleTTS(title, "en", 1)
+  googleTTS(title, "en", speed)
   // speed normal = 1 (default), slow = 0.24
   .then(function (url) {
     // https://translate.google.com/translate_tts?...
@@ -21,6 +40,6 @@ app.get('/voice/:title', function(req, res){
   });
 });
 
-const port = 5001
 console.log(`Voice server start on http://localhost:${port}`)
+console.log(`speed = ${speed}`)
 app.listen(port);
